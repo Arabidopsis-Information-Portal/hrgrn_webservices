fb413f57812b7c3f77963f57c8f5b88069fae4ab
Irina Belyaeva <ibelyaev@jcvi.org>
2015-12-27 21:37:55 -0500

    grn endpoints

---
+++ services/hrgrn_api/main.py
@@ -0,0 +1,8 @@
+# file: main.py
+import json
+
+def search(args):
+    print '[
+    {data: {id:'np13163',label:'4CL.1',type:'Protein',tftr:'',tips:'4CL.1; 4CL1; AT4CL1, AT1G51680, ID=np13163, Protein',locus:'AT1G51680',shape:'ellipse',background_color:'#FCFCFC',border_color:'#585858',color:'#000000'} }
+    ]'
+    print "---"
