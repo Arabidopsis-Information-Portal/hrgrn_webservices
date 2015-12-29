8b5df7b6a43bcd341c887a6a1c871bcb22151f60
Irina Belyaeva <ibelyaev@jcvi.org>
2015-12-29 15:52:08 -0500

    api update

--- services/hrgrn_api/node_info/main.py
+++ services/hrgrn_api/node_info/main.py
@@ -35,5 +35,5 @@ def transform_response(incoming_response):
     return demjson.decode(incoming_response)
 
 def build_payload(url):
-    response = requests.get(svc_url)
+    response = requests.get(url)
     return transform_response(response)
