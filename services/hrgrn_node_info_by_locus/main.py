51549b9bf3c4b81fe3fbb0003d437c3d1d848f28
Irina Belyaeva <ibelyaev@jcvi.org>
2015-12-29 18:49:25 -0500

    ` list implementation

--- services/hrgrn_node_info_by_locus/main.py
+++ services/hrgrn_node_info_by_locus/main.py
@@ -16,6 +16,8 @@ import demjson
 #    {data: {id:'np13163',label:'4CL.1',type:'Protein',tftr:'',tips:'4CL.1; 4CL1; AT4CL1, AT1G51680, ID=np13163, Protein',locus:'AT1G51680',shape:'ellipse',background_color:'#FCFCFC',border_color:'#585858',color:'#000000'} }
 # ] which is invalid JSON
 
+service_endpoint = 'http://plantgrn.noble.org/hrgrn/nodes?'
+
 def search(arg):
     geneID = arg['geneID']
     response_format = 'json'
@@ -32,7 +34,20 @@ def search(arg):
          print "HTTPError:", e.message
 
 def list(args):
-    raise Exception('Not implemented yet')
+    getAllGeneNodes()
+
+def getAllGeneNodes():
+    response_format = 'json'
+    svc_url = 'http://plantgrn.noble.org/hrgrn/nodes?listall=T&format=json' + response_format
+
+    try:
+            response = build_payload(svc_url)
+            print json.dumps(response)
+            print '---'
+    except ValueError as e:
+         print "ValueError:", e.message
+    except requests.exceptions.HTTPError as e:
+         print "HTTPError:", e.message
 
 def transform_response(incoming_response):
     return demjson.decode(incoming_response.text)
