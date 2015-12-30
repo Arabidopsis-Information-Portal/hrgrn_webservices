942c483116af77964ac7d177b553c345a5aa4c07
Irina Belyaeva <ibelyaev@jcvi.org>
2015-12-30 00:24:27 -0500

    added building svc url using genes parameters

--- services/hrgrn_subnetwork_by_locus/main.py
+++ services/hrgrn_subnetwork_by_locus/main.py
@@ -9,10 +9,10 @@ import urllib2
 import demjson
 
 def search(arg):
-    geneID = arg['genes']
+    genes = arg['genes']
     response_format = 'json'
 
-    svc_url = 'http://plantgrn.noble.org/hrgrn/nodes?foreignID=' + 'AT2G38470' + '&format=' + response_format
+    svc_url = build_svc_url(params)
 
     try:
             response = build_payload(svc_url)
@@ -34,3 +34,23 @@ def build_payload(url):
     r = requests.get(url, headers=headers)
     r.raise_for_status()
     return transform_response(r)
+
+def get_svc_base_url():
+    return 'http://plantgrn.noble.org/hrgrn/subnetwork?hasParams=T&'
+
+def get_svc_suffix_url():
+    svc_suffix = '&steps=2&pathalg=allSimplePaths&COMPOSITION_validated=T&COMPOSITION_predicted=T&MODIFY_validated=T&PPI_validated=T&CPI_validated=T&GENEEXPREGU_validated=T&SRNAREGU_validated=T&SRNAREGU_predicted=T&MOLTRANSPORT_validated=T&CHEMREACTION_validated=T&COEXP_predicted=T&COEXP_value=0.8&cutoffHubRels=100&format=json'
+    return svc_suffix
+
+def build_parameters(params):
+    separator = ";"
+    result = 'nodes=' + separator.join(params.split(','))
+    return result
+
+def build_query_url(params):
+    query_url = str(build_parameters(params)) + str(get_svc_suffix_url())
+    return query_url
+
+def build_svc_url(params):
+    url = str(get_svc_base_url()) + build_query_url(params)
+    return url
