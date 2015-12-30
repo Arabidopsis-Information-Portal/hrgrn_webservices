e32e63639af0300ff308f638a47204d7d599e953
Irina Belyaeva <ibelyaev@jcvi.org>
2015-12-29 19:28:52 -0500

    list update

--- services/hrgrn_node_info_by_locus/main.py
+++ services/hrgrn_node_info_by_locus/main.py
@@ -38,7 +38,7 @@ def list(args):
 
 def getAllGeneNodes():
     response_format = 'json'
-    svc_url = 'http://plantgrn.noble.org/hrgrn/nodes?listall=T&format=' + response_format
+    svc_url = 'http://plantgrn.noble.org/hrgrn/nodes?listall=T&format=json'
 
     try:
             response = build_payload(svc_url)
