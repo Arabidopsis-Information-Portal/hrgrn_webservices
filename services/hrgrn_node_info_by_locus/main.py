5cb4cbb652418f99087023750d0812b5f1991302
Irina Belyaeva <ibelyaev@jcvi.org>
2016-01-03 20:45:40 -0500

    metadata update

--- services/hrgrn_node_info_by_locus/main.py
+++ services/hrgrn_node_info_by_locus/main.py
@@ -27,7 +27,6 @@ log.setLevel(logging.INFO)
 #    {data: {id:'np13163',label:'4CL.1',type:'Protein',tftr:'',tips:'4CL.1; 4CL1; AT4CL1, AT1G51680, ID=np13163, Protein',locus:'AT1G51680',shape:'ellipse',background_color:'#FCFCFC',border_color:'#585858',color:'#000000'} }
 # ] which is invalid JSON
 
-service_endpoint = 'http://plantgrn.noble.org/hrgrn/nodes?'
 session = FuturesSession(max_workers=10)
 
 def search(arg):
