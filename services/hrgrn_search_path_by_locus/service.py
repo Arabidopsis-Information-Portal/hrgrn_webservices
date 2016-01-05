9a725c497623be80b0ce9e59a8edb732faa55af9
Irina Belyaeva <ibelyaev@jcvi.org>
2016-01-05 16:33:19 -0500

    updated reference to v0.7

--- services/hrgrn_search_path_by_locus/service.py
+++ services/hrgrn_search_path_by_locus/service.py
@@ -11,4 +11,4 @@ def get_svc_base_url():
     return 'http://plantgrn.noble.org/hrgrn/path'
 
 def gene_svc_url(url='https://api.araport.org/community/v0.3', namespace='hrgrn'):
-    return op.join(url, namespace, 'hrgrn_node_info_by_locus_v0.6', 'search')
+    return op.join(url, namespace, 'hrgrn_node_info_by_locus_v0.7', 'search')
